c057b49d6155ac5c8680da4ebf529513e245bd8c
Ramsey Chapin <ramsey@ramseychapin.com>
2019-02-19 05:24:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,6 @@ function addElementToBeginningOfArray (array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
-  array.push(element);
   array.unshift(element);
   return array;
 }
