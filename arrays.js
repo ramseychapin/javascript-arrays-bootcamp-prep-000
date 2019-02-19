af14c26e41e9e5ca7a90faca427a8984e32a454a
Ramsey Chapin <ramsey@ramseychapin.com>
2019-02-19 05:22:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,12 @@
 var chocolateBars = ["snickers", "hundred grand","kitkat", "skittles"];
+
+function addElementToBeginningOfArray (array, element) {
+  const result = [element, ...array];
+  return result;
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  array.push(element);
+  array.unshift(element);
+  return array;
+}
